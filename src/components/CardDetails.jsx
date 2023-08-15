157fad4ff5ae363ac530b050be42fbc54147d1c3
SalmanMK007 <129721857+SalmanMK007@users.noreply.github.com>
2023-08-15 15:36:42 +0500

    first commit

---
+++ src/components/CardDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./cartstyle.css"
+
+const CardDetails = () => {
+  return (
+    <div>CardDetails</div>
+  )
+}
+
+export default CardDetails
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./style.css"
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardsData from "./CardData";
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
