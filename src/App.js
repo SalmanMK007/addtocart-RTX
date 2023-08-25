e7de020a7a224e7b917d727c8b064b39a6f27e09
SalmanMK007 <129721857+SalmanMK007@users.noreply.github.com>
2023-08-25 17:14:03 +0500

    complete the project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Headers from './components/Headers';
 import Home from './components/Home';
+import CardDetails from './components/CardDetails';
+import { Routes,Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <>
     <Headers />
-    <Home />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='cart' element={<CardDetails />} />
+    </Routes>
+    
     </>
   );
 }
--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-// import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
 const Headers = () => {
+
+    const {carts}= useSelector((state)=> state.allCart );
+    // console.log(carts );
+    
     return (
         <>
             <Navbar style={{ height: "60px", background: "black", color: "white" }}>
                 <Container>
-                    {/* <NavLink className="text-decoration-none text-light mx-2"> */}
+                    <NavLink to="/" className="text-decoration-none text-light mx-2">
                         <h3 className='text-light'>Ecommerce</h3>
-                    {/* </NavLink> */}
-                    {/* <NavLink className="text-decoration-none text-light mx-2"> */}
+                    </NavLink>
+                    <NavLink to="/cart" className="text-decoration-none text-light mx-2">
                         <div id='ex4'>
-                            <span className='p1 fa-stack fa-2x has-badge' data-count={1}>
+                            <span className='p1 fa-stack fa-2x has-badge' data-count={carts.length}>
                                 <i class="fa-solid fa-cart-shopping"></i>
                             </span>
                         </div>
-                    {/* </NavLink> */}
+                    </NavLink>
 
                 </Container>
             </Navbar>
